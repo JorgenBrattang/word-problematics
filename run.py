a0f0cf2b54da64b7b8256390dcf4fd2157d2bc4f
JorgenBrattang <jorgen.kvist@gmail.com>
2022-08-15 16:51:08 +0000

    Add feature to convert questions number into integers

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+question = ["The cow has three tables"]
+
+
+def convert(data):
+    """
+    Converts a sentence into a list
+    """
+    return ([i for item in data for i in item.split()])
+     
+
+my_list = (convert(question))
+
+
+unique_numbers = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen",
+        ]
+
+new_list = []
+for x in range(len(my_list)):
+    if my_list[x] in unique_numbers:
+        new_list.append(unique_numbers.index(my_list[x]))
+    else:
+        new_list.append(my_list[x])
+
+print(question)
+print(new_list)
